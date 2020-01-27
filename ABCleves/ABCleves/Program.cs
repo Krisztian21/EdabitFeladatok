6af44fc0657e4f990d7c5ed0f7589f9f8d8f7013
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-01-27 12:57:15 +0100

    Mai

---
+++ ABCleves/ABCleves/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCleves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a szót: ");
+            string bemeno = Console.ReadLine();
+            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_1(bemeno)}");
+            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_2(bemeno)}");
+            Console.ReadLine();
+        }
+        static string Sorrendbe_1(string bemeno)
+        {
+            char[] tomb = bemeno.ToCharArray();
+            Array.Sort(tomb);
+
+            return new string(tomb);
+
+        }
+        static string Sorrendbe_2(string bemeno)
+        {
+            string kimeno = "{";
+            for(int i=0; i<bemeno.Length; i++)
+            {
+                for(int j=0; j<kimeno.Length; j++)
+                {
+                    if(bemeno[i]<kimeno[j])
+                    {
+                        kimeno = String.Concat(kimeno.Substring(0, j), bemeno[i], kimeno[j]);
+                        break;
+                    }
+                    kimeno = String.Concat(kimeno, bemeno[i]);
+                }
+            }
+            return kimeno.Substring(0,bemeno.Length);
+
+        }
+    }
+}
