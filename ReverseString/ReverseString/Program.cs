2d64dfe112365be9fe6158ca9a71927d69799bc7
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-01-27 11:55:45 +0100

    Mai 4

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a stringet: ");
+            string visszafele = Console.ReadLine();
+            Vissza megfordit = new Vissza(visszafele);
+            Console.WriteLine($"A string visszafelé: {Vissza(visszafele)}");
+            Console.WriteLine($"A string visszafelé: {megfordit.VisszaF()}");
+            Console.ReadLine();
+        }
+        static string Vissza(string visszafele)
+        {
+            string kimeno = "";
+            for(int i= visszafele.Length-1; i>=0; i--)
+            {
+                kimeno = String.Concat(kimeno, visszafele[i]);
+
+            }
+            return kimeno;
+        }
+    }
+}
---
+++ ReverseString/ReverseString/Vissza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Vissza
+    {
+        public string VisszaFele { get; set; }
+        public Vissza(string visszafele)
+        {
+            this.VisszaFele = visszafele;
+        }
+        public string VisszaF()
+        {
+            string kimeno = "";
+            for (int i = this.VisszaFele.Length - 1; i >= 0; i--)
+            {
+                kimeno = String.Concat(kimeno, this.VisszaFele[i]);
+
+            }
+            return kimeno;
+        }
+    }
+}
