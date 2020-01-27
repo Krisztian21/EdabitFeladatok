c7d6f46c8ff3ddbcb88fecb2b4ba6ff79c13e1cc
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-01-27 10:02:49 +0100

    Mai 2

---
+++ NTimes/NTimes/Ismetel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTimes
+{
+    class Ismetel
+    {
+        public string Bemeno { get; set; }
+
+        public Ismetel(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+        public string Ismeteles(string bemeno, int hanyszor)
+        {
+            string kimeno = "";
+            if (bemeno.Length == 0)
+            {
+                return kimeno = "Üres Stringgel nem tudok dolgozni.";
+            }
+            else
+            {
+                for (int i = 0; i < bemeno.Length; i++)
+                {
+                    for (int j = 0; j < hanyszor; j++)
+                    {
+                        kimeno = String.Concat(kimeno, bemeno[i]);
+                    }
+                }
+                return kimeno;
+            }
+        }
+    }
+}
---
+++ NTimes/NTimes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a Sztringet: ");
+            string bemeno = Console.ReadLine();
+            Console.Write("Kérem az ismétlés számát: ");
+            int hanyszor = Convert.ToInt32(Console.ReadLine());
+            Ismetel szoveg = new Ismetel(bemeno);
+            Console.WriteLine($"A ismételt string: {Ismetel(bemeno, hanyszor)}");
+            Console.WriteLine($"A ismételt string: {szoveg.Ismeteles(bemeno, hanyszor)}");
+            Console.ReadLine();
+        }
+        static string Ismetel(string bemeno, int hanyszor)
+        {
+            string kimeno="";
+            if(bemeno.Length==0)
+            {
+                return kimeno="Üres Stringgel nem tudok dolgozni.";
+            }
+            else
+            {
+                for(int i=0; i<bemeno.Length;i++)
+                {
+                    for(int j=0; j<hanyszor; j++)
+                    {
+                        kimeno=String.Concat(kimeno, bemeno[i]);
+                    }
+                }
+                return kimeno;
+            }
+        }
+    }
+}
