1d77bf060b7784b4848b82a20b0313447b93750f
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-02-04 09:04:53 +0100

    rggdsgs

--- ABCleves/ABCleves/Program.cs
+++ ABCleves/ABCleves/Program.cs
@@ -8,15 +8,19 @@ namespace ABCleves
 {
     class Program
     {
+        public enum Betuk { a = 1, á, b, c, d, e, é, f, g, h, i, í, j, k, l, m,
+            n, o, ó, ö, ő, p, q, r, s, t, u, ú, ü, ű, v, w, x, y, z, ß } //Magyar ABC megoldás
         static void Main(string[] args)
         {
             Console.Write("Kérem a szót: ");
             string bemeno = Console.ReadLine();
-            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_1(bemeno)}");
-            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_2(bemeno)}");
+            Szoveg szoveg = new Szoveg(bemeno);
+            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_1(bemeno)}");//ASCII code megoldás
+            Console.WriteLine($"A szót ábécé sorrendben: {Sorrendbe_2(bemeno)}");//Magyar ABC megoldás
+            Console.WriteLine($"A szót ábécé sorrendben: {szoveg.Sorrendbe_2()}");//Magyar ABC megoldás Osztály meg.
             Console.ReadLine();
         }
-        static string Sorrendbe_1(string bemeno)
+        static string Sorrendbe_1(string bemeno)//ASCII code megoldás
         {
             char[] tomb = bemeno.ToCharArray();
             Array.Sort(tomb);
@@ -24,22 +28,25 @@ namespace ABCleves
             return new string(tomb);
 
         }
-        static string Sorrendbe_2(string bemeno)
+        static string Sorrendbe_2(string bemeno)//Magyar ABC megoldás
         {
-            string kimeno = "{";
+            string kimeno = "ß";
             for(int i=0; i<bemeno.Length; i++)
             {
+                Betuk be;
+                Enum.TryParse(bemeno[i].ToString(), true, out be);
                 for(int j=0; j<kimeno.Length; j++)
                 {
-                    if(bemeno[i]<kimeno[j])
+                    Betuk ki;
+                    Enum.TryParse(kimeno[j].ToString(), true, out ki);
+                    if ((int)be<(int)ki)
                     {
-                        kimeno = String.Concat(kimeno.Substring(0, j), bemeno[i], kimeno[j]);
+                        kimeno = String.Concat(kimeno.Substring(0, j), bemeno[i], kimeno.Substring(j));
                         break;
                     }
-                    kimeno = String.Concat(kimeno, bemeno[i]);
                 }
             }
-            return kimeno.Substring(0,bemeno.Length);
+            return kimeno.Substring(0, bemeno.Length);
 
         }
     }
---
+++ ABCleves/ABCleves/Szoveg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCleves
+{
+    public enum Betuk { a = 1, á, b, c, d, e, é, f, g, h, i, í, j, k, l, m,
+               n, o, ó, ö, ő, p, q, r, s, t, u, ú, ü, ű, v, w, x, y, z, ß } //Magyar ABC megoldás
+    class Szoveg
+    {
+        public string Bemeno { get; set; }
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+        public string Sorrendbe_2()//Magyar ABC megoldás
+        {
+            string kimeno = "ß";
+            for (int i = 0; i < this.Bemeno.Length; i++)
+            {
+                Betuk be;
+                Enum.TryParse(this.Bemeno[i].ToString(), true, out be);
+                for (int j = 0; j < kimeno.Length; j++)
+                {
+                    Betuk ki;
+                    Enum.TryParse(kimeno[j].ToString(), true, out ki);
+                    if ((int)be < (int)ki)
+                    {
+                        kimeno = String.Concat(kimeno.Substring(0, j), this.Bemeno[i], kimeno.Substring(j));
+                        break;
+                    }
+                }
+            }
+            return kimeno.Substring(0, this.Bemeno.Length);
+
+        }
+    }
+}
